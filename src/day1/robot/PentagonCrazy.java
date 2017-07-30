08c47cde99870d4bc80523f200e6515c74a6aaa9
bartbacker <league@WTS20.attlocal.net>
2017-07-30 15:29:44 -0700

    Today, I had learned and practiced the basics of Java Script

--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -10,32 +10,38 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot c3po = new Robot();
 		// 3. Put the robot's pen down
-
+		c3po.penDown();
 		// 8. Make the robot go at maximum speed (10)
-
+		c3po.setSpeed(10);
 		// 9. Set the pen to a color that you like for the shape
-
-		// 4. Make a variable for the number of sides you want (can’t test this one)
-
-		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
-		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
+		c3po.setPenColor(Color.MAGENTA);
+		// 4. Make a variable for the number of sides you want (can’t test this
+		// one)
+		int sides = 5;
+		// 5. Make a variable for the angle you want the robot to turn. Hint:
+		// you can divide in Java using "/". Can’t test until step 6.
+		int angle = 360 / sides;
+		// 7. Do steps #2 to #11, 200 times. When this is done you should see a
+		// pentagon.
+		for (int i = 0; i < 200000; i++) {
 
 			// 2. Move the robot 200 pixels
-	
-			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
-	
-			// 6. Turn the robot the amount in your angle variable
-	
-			// 11. Turn the robot one more degree
+			c3po.move(i);
+			// 10. Make the robot move "i" pixels instead of 200 (don’t need new
+			// line of code for this, just change previous one)
 
+			// 6. Turn the robot the amount in your angle variable
+			c3po.turn(angle);
+			// 11. Turn the robot one more degree
+			c3po.turn(1);
+		}
 	}
 
 	// Variations:
 	// *12. make the pattern really huge
-	// *13. randomize the color of the pattern 
+	// *13. randomize the color of the pattern
 	// *14. experiment with different shapes
 
 	public static void main(String[] args) {
